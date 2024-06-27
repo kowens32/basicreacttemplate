4d24bd4394c79423a3abdcc356f9e35462ec008b
Kimberly Owens <kturner2232@gmail.com>
2024-06-27 12:32:30 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+// src/App.js
+
+import React from 'react';
+import { Container } from '@mui/material';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <Container maxWidth="lg">
+      <Dashboard />
+    </Container>
+  );
+}
+
+export default App;
